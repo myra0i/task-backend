e597996faf61e89cf419a1d20d4300ab52600b91
Mercy <mercymwangi07@gmail.com>
2024-01-29 18:13:34 +0300

    change made

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ const Pool = require('pg').Pool
 require('dotenv').config()
  
 const pool = new Pool({
-    user: 'postgres',
-    password: '0710682121',
+    user: 'database_4umt_user',
+    password: 'TS5QUY1WOAsAFmKfe1p0VcVPfmvAC6w7',
     host: process.env.HOST,
     port: process.env.DBPORT,  
-    database:'postgres' ,
+    database:'todo' ,
     ssl:{
         rejectUnauthorized: false, 
       }
